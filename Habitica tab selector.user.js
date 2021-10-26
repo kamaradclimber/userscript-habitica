e0b2c2695fc91353b23266ef844c3946003dde03
Grégoire Seux <kamaradclimber@gmail.com>
2021-10-26 17:30:13 +0200

    Add 1st working version
    Change-Id: I25be405697eda02cc0e2eaa244bd02996038ef2e

---
+++ Habitica tab selector.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         Habitica tab selector
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Automatically select proper tabs when opening habitica
+// @author       kamaradclimber
+// @match        https://habitica.com/
+// @icon         https://www.google.com/s2/favicons?domain=habitica.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+
+    var onTabAvailable = function (resolve) {
+        var filter = "div.filter";
+        var filters = document.querySelectorAll(filter);
+        if (filters && 0 < filters.length) {
+            filters.forEach(function(filter) {
+                resolve(filter)
+            })
+        } else {
+            window.setTimeout(function () {
+                console.log("Filters not visible, relaunching timer");
+                onTabAvailable(resolve);
+            }, 100);
+        }
+    }
+
+    onTabAvailable(function(filter) {
+        switch(filter.innerText) {
+            case "Due":
+            case "Scheduled":
+                filter.click();
+                break;
+        }
+    })
+
+    })();
